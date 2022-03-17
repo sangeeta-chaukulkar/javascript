726cda6840be8a6c5a05041303a503cd31f4b3cc
sangeeta-chaukulkar <91315460+sangeeta-chaukulkar@users.noreply.github.com>
2022-03-17 13:44:04 +0530

    added delete and edit functionality

---
+++ dom4.js
@@ -0,0 +1,35 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+console.log(form);
+console.log(itemList);
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+
+
+function addItem(e){
+  e.preventDefault();
+  var newItem = document.getElementById('item').value;
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+  var editbutton = document.createElement('button');
+  editbutton.className = 'btn btn-dark btn-sm float-right';
+  editbutton.appendChild(document.createTextNode('Edit'));
+  li.append(editbutton);
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
