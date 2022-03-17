26fbc35ad6a3052bc969ae579694147a8027af8e
sangeeta-chaukulkar <91315460+sangeeta-chaukulkar@users.noreply.github.com>
2022-03-17 14:48:21 +0530

    added filter

--- dom4.js
+++ dom4.js
@@ -1,18 +1,29 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 
+var filter = document.getElementById('filter');
+
 console.log(form);
 console.log(itemList);
 form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
 
 
 function addItem(e){
   e.preventDefault();
   var newItem = document.getElementById('item').value;
+  var newDesc=document.getElementById('description').value;
+  
   var li = document.createElement('li');
   li.className = 'list-group-item';
+  var newDiv =  document.createElement('p');
+  var newDivText = document.createTextNode(newDesc);
+  newDiv.appendChild(newDivText);
+
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(newDiv);
+ 
   var deleteBtn = document.createElement('button');
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
   deleteBtn.appendChild(document.createTextNode('X'));
@@ -33,3 +44,20 @@ function removeItem(e){
     }
   }
 }
+
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    //console.log(items); //HTMLCollection
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      var itemDesc = item.childNodes[1].textContent;
+      //console.log(itemDesc);
+      if(itemName.toLowerCase().indexOf(text) != -1 || itemDesc.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
